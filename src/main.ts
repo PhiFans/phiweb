db6b58b6fad5bb4dd6259530843e8b18fbcf0b48
Misa Liu <misaliu@misaliu.top>
2023-11-18 22:43:37 +0800

    feat: create a basic babylonjs app

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,30 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import { Engine } from '@babylonjs/core/Engines/engine';
+import { Scene } from '@babylonjs/core/scene';
+import { Vector3 } from '@babylonjs/core/Maths/math';
+import { FreeCamera } from '@babylonjs/core/Cameras/freeCamera';
+import { HemisphericLight } from '@babylonjs/core/Lights/hemisphericLight';
+import { Mesh } from '@babylonjs/core/Meshes/mesh';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import '@babylonjs/core/Materials/standardMaterial';
+import '@babylonjs/core/Meshes/Builders/sphereBuilder';
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+import './style.css';
+
+const canvas = document.querySelector<HTMLCanvasElement>('canvas#app')!;
+
+const engine = new Engine(canvas);
+let scene = new Scene(engine);
+
+let camera = new FreeCamera('camera', new Vector3(0, 5, -10), scene);
+camera.setTarget(Vector3.Zero());
+camera.attachControl(canvas, false);
+
+let light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+light.intensity = 0.7;
+
+let sphere = Mesh.CreateSphere('sphere', 16, 2, scene);
+sphere.position.y = 2;
+
+engine.runRenderLoop(() => {
+  scene.render();
+});

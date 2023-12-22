728e425b3913368326aafa0d777f6c5d3d47acc8
Misa Liu <misaliu@misaliu.top>
2023-12-22 23:36:06 +0800

    feat(game/chart): refine judgeline functions

--- src/game/chart/judgeline.ts
+++ src/game/chart/judgeline.ts
@@ -15,6 +15,13 @@ export class GameChartJudgeLine implements IPhiChartJudgeLine {
   eventLayers: Array<GameChartEventLayer>;
   floorPositions: Array<GameChartFloorPosition>;
 
+  positionX: number;
+  positionY: number;
+  alpha: number;
+  angle: number;
+  speed: number;
+  floorPosition: number;
+
   constructor({
     /* id, */
     texture
@@ -22,8 +29,63 @@ export class GameChartJudgeLine implements IPhiChartJudgeLine {
     // this.id = id;
     this.texture = texture;
 
-    this.notes = [];
-    this.eventLayers = [];
-    this.floorPositions = new Array<GameChartFloorPosition>();
+    this.notes = new Array<GameChartNote>();
+    this.eventLayers = new Array<GameChartEventLayer>();
+    this.floorPositions = new Array<GameChartFloorPosition>(); // NOTE: Should function(s) of calculating floorPosition be written in class?
+
+    // Init prop values
+    this.positionX = 0;
+    this.positionY = 0;
+    this.alpha = 0;
+    this.angle = 0;
+    this.speed = 1;
+    this.floorPosition = 0;
+  }
+
+  sortFloorPosition() {
+    return this.floorPositions.sort((a, b) => b.startTime - a.startTime);
+  }
+
+  calculateValues(time: number) {
+    this.calculateEventValues(time);
+    this.floorPosition = this.calculateFloorPosition(time, this.floorPosition);
+  }
+
+  private calculateEventValues(time: number) {
+    let posX = 0,
+      posY = 0,
+      alpha = 0,
+      angle = 0,
+      speed = 0;
+    
+    for (let i = 0, l = this.eventLayers.length; i < l; i++) {
+      const eventLayer = this.eventLayers[i];
+      
+      eventLayer.calculateValues(time);
+
+      posX += eventLayer.positionX;
+      posY += eventLayer.positionY;
+      alpha += eventLayer.alpha;
+      angle += eventLayer.angle;
+      speed += eventLayer.speed;
+    }
+
+    this.positionX = posX;
+    this.positionY = posY;
+    this.alpha = alpha;
+    this.angle = angle;
+    this.speed = speed;
+  }
+
+  // TODO: Add calculation with speed value later
+  private calculateFloorPosition(time: number, fallbackValue = 0) {
+    for (let i = 0, l = this.floorPositions.length; i < l; i++) {
+      const floorPosition = this.floorPositions[i];
+
+      if (floorPosition.startTime < time) continue;
+      else return floorPosition.floorPosition;
+    }
+
+    return fallbackValue;
   }
 }

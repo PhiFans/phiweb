b5eca683c70ae9ab96635dd3b519f113c7b2be23
Misa Liu <misaliu@misaliu.top>
2024-11-14 16:05:23 +0800

    chore(vite): Use external chunk split plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import { resolve } from 'node:path';
 import { defineConfig, UserConfig } from 'vite';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 
 export default defineConfig(({ mode }) => {
   const baseConfig: UserConfig = {
+    plugins: [
+      chunkSplitPlugin({
+        strategy: 'default',
+      }),
+    ],
     resolve: {
       alias: {
         '@': resolve(__dirname, './src'),
@@ -10,13 +16,6 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       minify: mode === 'production' ? 'esbuild' : false,
-      rollupOptions: {
-        output: {
-          manualChunks: (id) => {
-            if (id.includes('node_modules')) return 'vendor';
-          },
-        },
-      },
     },
   };
 

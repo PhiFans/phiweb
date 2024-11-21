0b59459f64245b75624c335b46f87b6ca2cbcc90
Misa Liu <misaliu@misaliu.top>
2024-11-21 23:25:33 +0800

    feat(audio): Add game audio

---
+++ src/audio/index.ts
@@ -0,0 +1,61 @@
+import { Ticker } from 'pixi.js';
+import { GameAudioClock } from './clock';
+import { resumeAudioCtx } from './utils';
+import { GameAudioChannel } from './channel';
+
+const AudioCtx = window.AudioContext || window.webkitAudioContext;
+const GlobalAudioCtx = new AudioCtx({ latencyHint: 'interactive' });
+const GlobalAudioTicker = new Ticker();
+const GlobalAudioClock = new GameAudioClock(GlobalAudioCtx, GlobalAudioTicker);
+
+export class GameAudio {
+  readonly audioCtx: AudioContext;
+  readonly clock: GameAudioClock;
+
+  readonly masterGain: GainNode;
+  readonly channels: {
+    music: GameAudioChannel,
+    effect: GameAudioChannel,
+  };
+
+  constructor() {
+    this.audioCtx = GlobalAudioCtx;
+    this.clock = GlobalAudioClock;
+
+    this.masterGain = this.audioCtx.createGain();
+    this.masterGain.connect(this.audioCtx.destination);
+    this.channels = {
+      music: new GameAudioChannel(this, GlobalAudioTicker),
+      effect: new GameAudioChannel(this, GlobalAudioTicker),
+    };
+  }
+
+  get masterVolume() {
+    return this.masterGain.gain.value;
+  }
+
+  set masterVolume(volume: number) {
+    this.masterGain.gain.value = volume;
+  }
+}
+
+// Automatically resume AudioContext
+const resumeAudio = () => resumeAudioCtx(GlobalAudioCtx);
+const handleWindowLoaded = () => {
+  window.removeEventListener('load', handleWindowLoaded);
+
+  if (GlobalAudioCtx.state === 'running') return;
+  window.addEventListener('pointerdown', resumeAudio);
+  window.addEventListener('pointerover', resumeAudio);
+  window.addEventListener('pointerleave', resumeAudio);
+};
+
+GlobalAudioCtx.addEventListener('statechange', () => {
+  if (GlobalAudioCtx.state !== 'running') return;
+
+  console.info('[Audio]', 'Resume audio success');
+  window.removeEventListener('pointerdown', resumeAudio);
+  window.removeEventListener('pointerover', resumeAudio);
+  window.removeEventListener('pointerleave', resumeAudio);
+});
+window.addEventListener('load', handleWindowLoaded);
--- src/global.d.ts
+++ src/global.d.ts
@@ -3,6 +3,10 @@
 import type { Application, Container, AbstractRenderer } from "pixi.js";
 
 declare global {
+  interface Window {
+    webkitAudioContext: AudioContext,
+  }
+
   // For Devtool
   var __PIXI_APP__ = new Application;
   var __PIXI_STAGE__ = new Container;

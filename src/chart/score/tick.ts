e5d1e6d2d9a3369211bf788eb8ff8a4d9a18cdba
Misa Liu <misaliu@misaliu.top>
2024-12-04 13:06:59 +0800

    feat(chart/score): Add drag/flick note judge

--- src/chart/score/tick.ts
+++ src/chart/score/tick.ts
@@ -19,12 +19,10 @@ export function onScoreTick(this: GameChartScore, currentTime: number) {
   const { list: inputList } = inputs;
 
   judges.length = 0;
-  if (!isAutoPlay) {
-    for (const input of inputList) {
-      if (!input.isTapped) judges[judges.length] = new GameChartScoreJudge(1, input.x, input.y);
-      if (input.isFlickable && !input.isFlicked) judges[judges.length] = new GameChartScoreJudge(2, input.x, input.y, input);
-      judges[judges.length] = new GameChartScoreJudge(3, input.x, input.y);
-    }
+  if (!isAutoPlay) for (const input of inputList) {
+    if (!input.isTapped) judges[judges.length] = new GameChartScoreJudge(1, input.x, input.y, input);
+    if (input.isFlickable && !input.isFlicked) judges[judges.length] = new GameChartScoreJudge(2, input.x, input.y, input);
+    judges[judges.length] = new GameChartScoreJudge(3, input.x, input.y);
   }
 
   for (const note of notes) {
@@ -91,6 +89,45 @@ export function onScoreTick(this: GameChartScore, currentTime: number) {
         }
 
         this.updateScore(score.score);
+        if (input) input.isTapped = true;
+        judges.splice(i, 1);
+        break;
+      }
+    } else if (type === 2) {
+      if (score.isScored && score.score !== EGameChartScoreJudgeType.MISS && timeBetween <= 0) {
+        sprite!.visible = false;
+        score.isScoreAnimated = true;
+        this.updateScore(score.score);
+      } else for (let i = 0; i < judges.length; i++) {
+        const { type, x, y } = judges[i];
+
+        if (type !== 3) continue;
+        if (!isInArea(x, y, realPosX, realPosY, judgeline.cosr, judgeline.sinr, size.noteWidth)) continue;
+
+        score.score = EGameChartScoreJudgeType.PERFECT;
+        score.isScored = true;
+        score.timeBetween = NaN;
+
+        break;
+      }
+    } else if (type === 3) {
+      // TODO: Hold judge
+    } else if (type === 4) {
+      if (score.isScored && score.score !== EGameChartScoreJudgeType.MISS && timeBetween <= 0) {
+        sprite!.visible = false;
+        score.isScoreAnimated = true;
+        this.updateScore(score.score);
+      } else for (let i = 0; i < judges.length; i++) {
+        const { type, x, y, input } = judges[i];
+
+        if (type !== 2) continue;
+        if (!isInArea(x, y, realPosX, realPosY, judgeline.cosr, judgeline.sinr, size.noteWidth)) continue;
+
+        score.score = EGameChartScoreJudgeType.PERFECT;
+        score.isScored = true;
+        score.timeBetween = NaN;
+
+        if (input) input.isFlicked = true;
         judges.splice(i, 1);
         break;
       }

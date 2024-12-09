def2c77256ce7e467249831f858dce47db1e5b68
Misa Liu <misaliu@misaliu.top>
2024-12-09 20:36:33 +0800

    feat(skin/types): Remove unused types

--- src/skins/types.ts
+++ src/skins/types.ts
@@ -85,44 +85,6 @@ export type TGameSkinElement = (
   TGameSkinElementHitEffect
 );
 
-export interface IGameSkinElementBase {
-  type: TGameSkinElementType,
-  enabled: boolean,
-  align: 'left' | 'center' | 'right',
-  stickTo: {
-    x: 'left' | 'center' | 'right',
-    y: 'top' | 'center' | 'bottom',
-  },
-  position: IGameSkinElementCoordinate,
-  scale: number,
-}
-
-export interface IGameSkinElementTexture extends IGameSkinElementBase {
-  type: 'combo-text' | 'image',
-  path: string,
-  anchor: IGameSkinElementCoordinate,
-}
-
-export interface IGameSkinElementTextureNumber extends IGameSkinElementBase {
-  type: 'score' | 'accurate' | 'combo',
-  path: string,
-}
-
-export interface IGameSkinElementTextureAnimated extends IGameSkinElementBase {
-  type: 'hit-effect' | 'image',
-  path: string,
-  anchor: IGameSkinElementCoordinate,
-  speed: number,
-}
-
-export interface IGameSkinElementText extends IGameSkinElementBase {
-  type: 'song-name' | 'song-level' | 'song-artist' | 'text',
-  fontFamily: string,
-  size: number,
-}
-
-export type IGameSkinElement = IGameSkinElementTexture | IGameSkinElementTextureNumber | IGameSkinElementTextureAnimated | IGameSkinElementText;
-
 export interface IGameSkinElementCoordinate {
   x: number,
   y: number,

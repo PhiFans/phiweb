27fa034ba84cb18b85f4f89cd25165ee4eff962d
Misa Liu <misaliu@misaliu.top>
2024-12-10 00:36:45 +0800

    feat(score/ui): Rewrite score UI init

--- src/score/ui.ts
+++ src/score/ui.ts
@@ -1,51 +1,41 @@
 import { IGameRendererSize } from '@/renderer';
-import { GameSkinFiles } from '@/skins/file';
-import { IGameSkinElement, IGameSkinElementCoordinate, TGameSkinElementType } from '@/skins/types';
+import { GameSkin } from '@/skins';
+import { TGameSkinElement } from '@/skins/types';
 import { GameUITexturedNumber } from '@/ui/textured-number';
 import { parseDoublePrecist } from '@/utils/math';
-import { Container, Sprite } from 'pixi.js';
+import { Container /** , Sprite */ } from 'pixi.js';
 
-interface IGameScoreUIElement {
-  type: TGameSkinElementType,
-  stickTo: {
-    x: 'left' | 'center' | 'right',
-    y: 'top' | 'center' | 'bottom',
-  },
-  position: IGameSkinElementCoordinate,
-  scale: number,
+type TGameScoreUIElement = TGameSkinElement & {
   sprite: GameUITexturedNumber /** | Sprite */,
-}
-
-const getElementProps = (element: IGameSkinElement) => ({
-  type: element.type,
-  stickTo: element.stickTo,
-  position: element.position,
-  scale: element.scale,
-});
+};
 
 const accurateToText = (number: number) => `${parseDoublePrecist(number * 100, 2)}%`;
 
 export class GameScoreUI {
   readonly container = new Container();
-  readonly elements: IGameScoreUIElement[] = [];
+  readonly elements: TGameScoreUIElement[] = [];
 
-  constructor(elements: IGameSkinElement[], skinFiles: GameSkinFiles, container: Container, size: IGameRendererSize) {
-    this.elements = [ ...elements ].filter(e => e.enabled).map<IGameScoreUIElement | undefined>((e) => {
-      switch (e.type) {
-        case 'score':
-        case 'combo':
-        case 'accurate': {
-          return {
-            ...getElementProps(e),
-            sprite: new GameUITexturedNumber(skinFiles.numbers[e.type], e, e.type === 'score' ? 7 : 0),
+  constructor(skin: GameSkin, container: Container, size: IGameRendererSize) {
+    this.elements = [ ...skin.elements ]
+      .filter(e => e.type !== 'hit-effect')
+      .filter(e => e.enabled)
+      .map<TGameScoreUIElement | undefined>((e) => {
+        switch (e.type) {
+          case 'score':
+          case 'combo':
+          case 'accurate': {
+            return {
+              ...e,
+              sprite: new GameUITexturedNumber(e, e.type === 'score' ? 7 : 0),
+            }
+          };
+          default: {
+            console.warn(`No such element type: ${e.type}, skipping...`);
+            return (void 0);
           }
-        };
-        default: {
-          console.warn(`No such element type: ${e.type}, skipping...`);
-          return (void 0);
         }
-      }
-    }).filter((e) => (e !== (void 0)));
+      })
+      .filter((e) => (e !== (void 0)));
 
     for (const e of this.elements) this.container.addChild(e.sprite);
     container.addChild(this.container);
@@ -57,6 +47,8 @@ export class GameScoreUI {
     const { heightPercent, width, widthHalf, height, heightHalf } = size;
 
     for (const e of elements) {
+      if (e.type === 'hit-effect') continue;
+
       const { stickTo, position, scale, sprite } = e;
       const posX = (
         stickTo.x === 'left' ? position.x * heightPercent :

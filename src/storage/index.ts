afb4967c5be0749fdc7013f91732bcc8fee281ab
Misa Liu <misaliu@misaliu.top>
2024-12-21 00:26:54 +0800

    feat(storage): Add `addFile` method

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -1,4 +1,11 @@
 import { GameDatabase } from '@/database';
+import { getFileMD5 } from '@/utils/file';
+
+export type TGameDBFile = {
+  md5: string,
+  filename: string,
+  blob: Blob,
+};
 
 export class GameStorage {
   readonly dbFile: GameDatabase;
@@ -7,9 +14,20 @@ export class GameStorage {
     this.dbFile = new GameDatabase('file_db', 1, {
       structures: [
         { name: 'md5', options: { key: true } },
-        { name: 'filename', options: { index: true, unique: true } },
+        { name: 'filename', options: { index: true, unique: false } },
         { name: 'blob' },
       ]
     });
   }
+
+  addFile(filename: string, blob: Blob): Promise<{ md5: string, filename: string }> {return new Promise(async (res, rej) => {
+    const md5 = await getFileMD5(blob);
+    const fileData = await this.dbFile.get<TGameDBFile>(md5);
+    if (fileData) return res(fileData);
+
+    this.dbFile.add({
+      md5, filename, blob
+    }).then(() => res({ md5, filename }))
+      .catch(e => rej(e));
+  })}
 }
